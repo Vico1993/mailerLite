8c98b0d341569e2a63dd8011c00978a300fe727f
Victor Piolin <victor.piolin@gmail.com>
2018-04-12 18:49:26 -0700

    Added all routes for field Management

--- app/routes/FieldRoutes.php
+++ app/routes/FieldRoutes.php
@@ -5,6 +5,25 @@
  */
 $app->post( '/field/add', Controller\FieldController::class. ':add' );
 
+/**
+ * Update a field
+ */
+$app->put( '/field/update/{id}', Controller\FieldController::class. ':update' );
+
+/**
+ * Routes to get one or all field.
+ */
+$app->get( '/subscriber/get[/{id}]', Controller\FieldController::class. ':get' );
+
+/**
+ * Routes to get all field with a specific state and specif user
+ */
+$app->get( '/subscriber/filter[/{id_state}/subscriber/{id_subscriber}]', Controller\FieldController::class. ':filters');
+
+/**
+ * Routes to delete a Field
+ */
+$app->delete( '/subscriber/delete/{id}', Controller\FieldController::class. ':delete');
 
 
 ?>
